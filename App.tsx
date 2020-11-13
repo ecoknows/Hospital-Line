8c07a325e55d43384ae45aabb7fce3807ca8eb3a
ecoknows <66323903+ecoknows@users.noreply.github.com>
2020-11-13 13:19:28 +0800

    eco

--- App.tsx
+++ App.tsx
@@ -1,21 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Magnetometer } from 'expo-sensors';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const initialState = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
+
+
+
+export default function Main(){
+  const [currentLocation,  setCurrentLocation] = useState(initialState);
+  
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(()=>{
+   
+
+    navigator.geolocation.watchPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setCurrentLocation({
+          latitude,
+          longitude,
+        });
+        },
+      error => alert(error.message),
+      { timeout : 20000, maximumAge: 1000}
+    )
+
+    setCurrentLocation({
+      ...currentLocation,
+      latitude: 37.78825,
+      longitude: -122.4324,
+    });
+    
+    _toggle();
+    return () => {
+      _unsubscribe();
+    };
+  },[]);
+
+
+  Magnetometer.setUpdateInterval(16);
+  
+  const _toggle = () => {
+    if (subscription) {
+      _unsubscribe();
+    } else {
+      _subscribe();
+    }
+  };
+
+
+  const _subscribe = () => {
+    setSubscription(
+      Magnetometer.addListener(result => {
+        setData(result);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  const { x, y, z } = data;
+  
+  
+
+  let angle = Math.atan2(round(y), round(x));
+  angle = angle * (180 / Math.PI)
+  angle = angle + 90
+  angle = (angle +360) % 360 
+  
+  return(
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.container}
+      initialRegion={currentLocation}
+    >
+      <Marker
+        coordinate={currentLocation}
+        image={require('./assets/bee.png')}
+        style={{transform: [{
+          rotate: `${Math.round(angle)}`+'deg',
+        }]}}
+      >
+
+      </Marker>
+    </MapView>
   );
 }
 
+
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+  return Math.floor(n * 100) / 100;
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
